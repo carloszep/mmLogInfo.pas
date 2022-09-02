89656be6ba20e62d7c21b0c88b12ec5e717e7dd3
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2022-09-01 20:47:13 -0500

    updated documentation in dlgRead unit

--- unit/dlgRead.pp
+++ unit/dlgRead.pp
@@ -5,16 +5,87 @@
 |     _ Autodock 4.2 .dlg files .
 |    -intended to be used by the mmLogInfo program .
 |    -.dlg file info... :
-|      -relevant information :
+|      -relevant information (example lines) :
 |        -program version :
-|          -example line :
-|            -"          AutoDock 4.2 Release 4.2.6" ;;
-|        -grid center :
-|          -example line :
-|            -"Coordinates of Central Grid Point of Maps 
-|             _=     (4.620, 66.120, 62.720)" ;;;;;
-|  -version :-0.0.1 ;
-|  -authors :-Carlos Z. Gómez Castro ;
+|          -"          AutoDock 4.2 Release 4.2.6" ;
+|        -job info :
+|          -"This file was created at:     7:09 31" p.m., 05/23/2022" .
+|          -"                   on host:   "nodo5"" .
+|          -"Current Working Directory
+|           _ = "/home/czgomez/autodock4/ache/carba4/dock/2bagA-GSG1200"" ;
+|        -dpf file name :
+|          -"Docking parameter file (DPF) used for this docking:
+|           _   0071119103-HCC_2bag.dpf" ;
+|        -grid point spacing :
+|          -"Grid Point Spacing =        0.375 Angstroms" ;
+|        -number of grid point :
+|          -"Even Number of User-specified Grid Points = 90 x-points" .
+|          -"            90 y-points" .
+|          -"            90 z-points" ;
+|        -grid center coordinates :
+|          -"Coordinates of Central Grid Point of Maps
+|           _ = (3.970, 65.100, 62.660)" ;
+|        -macromolecule name :
+|          -"Macromolecule file used to create Grid Maps = 2bag.pdbqt" ;
+|        -grid parameter file :
+|          -"Grid Parameter file used to create Grid Maps =  2bag.gpf" ;
+|        -minimum grid coordinates :
+|          -"Minimum coordinates in grid = (-12.905, 48.225, 45.785)" ;
+|        -maximum grid coordinates :
+|          -"Maximum coordinates in grid = (20.845, 81.975, 79.535)" ;
+|        -ligand pdbqt file name :
+|          -"Ligand PDBQT file = "0071119103-HCC.pdbqt"" ;
+|        -ligand pdbqt file... .
+|        -number of runs :
+|          -"DPF> ga_run 100" .
+|          -"Number of requested GA dockings = 100 runs" ;
+|        -genetic algorithm docking start :
+|          -"  BEGINNING GENETIC ALGORITHM DOCKING 1 of 100" ;
+|        -analysis sections :
+|          -"DPF> analysis" .
+|          -"    CLUSTER ANALYSIS OF CONFORMATIONS" .
+|          -"  CLUSTERING HISTOGRAM" .
+|          -"  RMSD TABLE" .
+|          -"  INFORMATION ENTROPY ANALYSIS FOR THIS CLUSTERING" .
+|          -"  STATISTICAL MECHANICAL ANALYSIS" .
+|          -"  LOWEST ENERGY DOCKED CONFORMATION from EACH CLUSTER" ;
+|        -docked conformation information from each cluster :
+|          -"MODEL        6" .
+|          -"USER    Run = 6" .
+|          -"USER    Cluster Rank = 1" .
+|          -"USER    Number of conformations in this cluster = 24" .
+|          -"USER    RMSD from reference structure       = 94.009 A" .
+|          -"USER    Estimated Free Energy of Binding    =   -7.88 kcal/mol" .
+|          -"USER    Estimated Inhibition Constant, Ki
+|           _   =    1.66 uM (micromolar)" .
+|          -"USER    NEWDPF tran0  2.404770 65.639632 68.722938" .
+|          -"USER                              x       y       z
+|           _    vdW   Elec        q     RMS"
+|          -"ATOM      1  C   LIG A   1       1.696  68.482  67.926
+|           _ -0.36 -0.10    +0.085    94.009" .
+|          -"TER" .
+|          -"ENDMDL" ;
+|        -final remarks :
+|          -">>> Closing the docking parameter file (DPF)..." .
+|          -"This docking finished at:     7:40 04" p.m., 05/23/2022" .
+|          -"/home/czgomez/software/autodock/src/autodock/autodock4:
+|           _ Successful Completion on "nodo5"" .
+|          -"Real= 30m 33.15s,  CPU= 30m 32.96s,  System= 0.23s" ;;
+|      -information that can be calculated or extracted :
+|        -list of distances between each conformation and the grid center .
+|        -list of energies .
+|        -list of inhibition constants .
+|        -list of RMSDs from reference structure .
+|        -completition status .
+|        -computation time ;;
+|    -information to be output in a molInfo tcl script file :
+|      -a list of dir paths containing one or more dlg files (grid names) .
+|      -list of dlg file names in each dir .
+|      -list of energies of each conformation either from clusters or runs .
+|      -list of distances between centers of conformations and grid .
+|      -list of RMSD relative to the reference ;
+|    -version :-0.0.1 ;
+|    -authors :-Carlos Z. Gómez Castro ;
 |  -version information :
 |    -date :-2022-08-18.Thr ;
 |    -status :-unfinished ;
