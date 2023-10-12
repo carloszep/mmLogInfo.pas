e19db0db0083fdbd9e8b7965e6e0562b801fd989
Carlos Z. Gómez Castro <carloszepgc@gmail.com>
2023-10-12 13:08:42 -0600

    compiled genDelPhiParameters for linux

---
+++ prog/genDelPhiParameters.pp
@@ -0,0 +1,25 @@
+program genDelPhiParameters.pp;
+
+{$H+}
+
+uses
+  condText, charmmRead;
+
+var
+  pNode : p_CTnode;
+  ctInp : obj_condText;
+
+begin
+
+  ctInp.init;
+  pNode := nil;
+
+  config_genDelPhiParam (ctInp, pNode);
+{input information for charmmRead}
+{  ctInp.open ('charmmRead_extUsrOpts.ct', 'I');}
+{  pNode := ctInp.findTextRec (nil, 'genDelPhiParam');}
+{  if pNode <> nil then}
+{    writeln ('genDelPhiParam node found in ctInp...');}
+{  genDelPhiParam (ctInp, pNode);}
+
+end.
